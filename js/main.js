d15b504729fe28b30e538523eb8db07f5edc4486
CzechVadim <139116076+CzechVadim@users.noreply.github.com>
2025-05-05 15:02:47 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('.variable-width').slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true
+});
+  
